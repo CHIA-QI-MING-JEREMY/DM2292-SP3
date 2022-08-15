f8664253e10b38c78375a4954ad9a2a2aaf043ec
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2022-08-15 12:01:27 +0800

    Revert "Update csettings add function x"
    This reverts commit aa0fe90f1c8c1edc56ca5eb3641f457673b4414b.

--- NYP_Framework/Library/Source/GameControl/Settings.h
+++ NYP_Framework/Library/Source/GameControl/Settings.h
@@ -76,7 +76,6 @@ public:
 
 	// Convert an index number of a tile to a coordinate in UV Space
 	float ConvertIndexToUVSpace(const AXIS sAxis, const int iIndex, const bool bInvert, const float fOffset = 0.0f);
-	glm::vec2 ConvertIndexToUVSpace(const glm::vec2 pos);
 
 protected:
 	// Constructor
